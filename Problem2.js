071905d1dc72e79b5536fea358bf06db00065e8d
Akshya20 <146994228+Akshya20@users.noreply.github.com>
2025-05-25 14:51:56 +0530

    Update Problem2.js

--- Problem2.js
+++ Problem2.js
@@ -1,9 +1,9 @@
-function OddLine(x){
+function Sequence(num){
     let res="";
     let i=1;
     let count=0;
     
-    while(count<x){
+    while(count<num){
       if(i%2!==0){
           res+=i;
           count++;
@@ -12,5 +12,5 @@ function OddLine(x){
     }
     return res;
 }
-let u=3;
-console.log(OddLine(u));
+let target=3;
+console.log(Sequence(target));
